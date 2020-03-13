119a91d578ba169dac0aa4f0a968241891fe4e4c
hilmanauliarahman <hilman.ra@students.amikom.ac.id>
2020-03-13 12:54:30 +0700

    praktikum02 final hilman 19.11.2656

---
+++ kalkulator.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator
+{
+	class Calculator
+	{
+		static void Main(string[] args)
+		{
+			int a;
+			int b;
+			int total = 0;
+
+			Console.WriteLine("Aplikasi Calculator");
+			Console.WriteLine();
+			Console.WriteLine("1.Penambahan");
+			Console.WriteLine("2.Pengurangan");
+			Console.WriteLine("3.Perkalian");
+			Console.WriteLine("4.Pembagian");
+			Console.WriteLine();
+			Console.Write("Masukan Pilihan[1-4]:");
+			int pilih = Int32.Parse(Console.ReadLine());
+
+			if (pilih == 1)
+			{
+				Console.Write("Inputkan Nilai A = ");
+				a = int.Parse(Console.ReadLine());
+				Console.Write("Inputkan Nilai B = ");
+				b = int.Parse(Console.ReadLine());
+				Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+			}
+			else if (pilih == 2)
+			{
+				Console.Write("Inputkan Nilai A = ");
+				a = int.Parse(Console.ReadLine());
+				Console.Write("Inputkan Nilai B = ");
+				b = int.Parse(Console.ReadLine());
+				Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+			}
+			else if (pilih == 3)
+			{
+				Console.Write("Inputkan Nilai A = ");
+				a = int.Parse(Console.ReadLine());
+				Console.Write("Inputkan Nilai B = ");
+				b = int.Parse(Console.ReadLine());
+				Console.WriteLine("Hasil Perkalian: {0} * {1} = {2}", a, b, Perkalian(a, b));
+			}
+			else if (pilih == 4)
+			{
+				Console.Write("Inputkan Nilai A = ");
+				a = int.Parse(Console.ReadLine());
+				Console.Write("Inputkan Nilai B = ");
+				b = int.Parse(Console.ReadLine());
+				Console.WriteLine("Hasil Pembagian: {0} / {1} = {2}", a, b, Pembagian(a, b));
+			}
+			else
+			{
+				Console.WriteLine("Tidak ada pilihan");
+			}
+			Console.WriteLine("\nTekan sembarang key untuk keluar");
+			Console.ReadKey();
+		}
+
+		static int Penambahan(int a, int b)
+		{
+			return a + b;
+		}
+
+		static int Pengurangan(int a, int b)
+		{
+			return a - b;
+		}
+
+		static int Perkalian(int a, int b)
+		{
+			return a * b;
+		}
+
+		static int Pembagian(int a, int b)
+		{
+			return a / b;
+		}
+
+	}
+}
